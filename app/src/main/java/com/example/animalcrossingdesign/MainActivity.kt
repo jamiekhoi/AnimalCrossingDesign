7f88dd90ead328f87128ec846b656d62e28b6b66
James Giang <james.k.giang@gmail.com>
2021-05-10 16:11:44 +0200

    Add login button in top toolbar for whole app

--- app/src/main/java/com/example/animalcrossingdesign/MainActivity.kt
+++ app/src/main/java/com/example/animalcrossingdesign/MainActivity.kt
@@ -17,6 +17,18 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.animalcrossingdesign.firebaseuiloginsample.LoginViewModel
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +36,12 @@ class MainActivity : AppCompatActivity() {
 
     private val READ_EXTERNAL_STORAGE_REQUEST_CODE: Int = 200
     private val WRITE_EXTERNAL_STORAGE_REQUEST_CODE: Int = 201
+    private val SIGN_IN_REQUEST_CODE = 10
+    private val TAG = "MainActivity"
+
+    // Get a reference to the ViewModel scoped to this Fragment
+    private val viewModel by viewModels<LoginViewModel>()
+    private lateinit var loginButton: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +49,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        observeAuthenticationState()
+        loginButton = findViewById(R.id.auth_button)
+        loginButton.setOnClickListener { launchSignInFlow() }
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
@@ -113,5 +135,99 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // TODO Listen to the result of the sign in process by filter for when
+        //  SIGN_IN_REQUEST_CODE is passed back. Start by having log statements to know
+        //  whether the user has signed in successfully
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in.
+                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            } else {
+                // Sign in failed. If response is null, the user canceled the
+                // sign-in flow using the back button. Otherwise, check
+                // the error code and handle the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+
+    }
+
+    /**
+     * Observes the authentication state and changes the UI accordingly.
+     * If there is a logged in user: (1) show a logout button and (2) display their name.
+     * If there is no logged in user: show a login button
+     */
+    private fun observeAuthenticationState() {
+
+        // TODO Use the authenticationState variable from LoginViewModel to update the UI
+        //  accordingly.
+        //
+        //  TODO If there is a logged-in user, authButton should display Logout. If the
+        //   user is logged in, you can customize the welcome message by utilizing
+        //   getFactWithPersonalition(). I
+
+        // TODO If there is no logged in user, authButton should display Login and launch the sign
+        //  in screen when clicked. There should also be no personalization of the message
+        //  displayed.
+        //viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+        viewModel.authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    loginButton.text = getString(R.string.logout_button_text)
+
+                    loginButton.setOnClickListener {
+                        // TODO implement logging out user in next step
+                        //AuthUI.getInstance().signOut(requireContext())
+                        AuthUI.getInstance().signOut(this)
+                    }
+
+                    // TODO 2. If the user is logged in,
+                    // you can customize the welcome message they see by
+                    // utilizing the getFactWithPersonalization() function provided
+                    //binding.welcomeText.text = getFactWithPersonalization(factToDisplay)
+
+
+                }
+                else -> {
+                    // TODO 3. Lastly, if there is no logged-in user,
+                    // auth_button should display Login and
+                    //  launch the sign in screen when clicked.
+                    loginButton.text = getString(R.string.login_button_text)
+                    loginButton.setOnClickListener { launchSignInFlow() }
+                    //binding.welcomeText.text = factToDisplay
+                }
+            }
+        })
+    }
+
+    private fun launchSignInFlow() {
+        // TODO Complete this function by allowing users to register and sign in with
+        //  either their email address or Google account.
+        // Give users the option to sign in / register with their email or Google account.
+        // If users choose to register with their email,
+        // they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+
+            // This is where you can provide more ways for users to register and
+            // sign in.
+        )
+
+        // Create and launch the sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_REQUEST_CODE.
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                //.setIsSmartLockEnabled(false)
+                .build(),
+            SIGN_IN_REQUEST_CODE
+        )
     }
 }
